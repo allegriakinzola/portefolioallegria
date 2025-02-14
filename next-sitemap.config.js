d881855cada78130944245544ff7e669d1f8a7b4
allegriakinzola <allegriakinzoladev@gmail.com>
2025-02-15 00:26:13 +0100

    generations des fichiers

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://belela.tech', // Remplace par l'URL de ton site
+    generateRobotsTxt: true, // Génère aussi un fichier robots.txt
+    sitemapSize: 5000, // Nombre max d'URL par fichier sitemap
+  };
+  
